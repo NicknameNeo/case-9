bb2c88247e84441e49a309c3c0aab8092cb469b5
NicknameNeo <abdulloevaslan96@gmail.com>
2024-11-27 18:54:53 +0500

    my project

---
+++ Case 9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Case_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day = int.Parse(Console.ReadLine());
+            var month = int.Parse(Console.ReadLine());
+            int daysInMonth = 0;
+            switch (month)
+            {
+                case 1:case 3:case 5:case 7: case 10:case 12:
+                    daysInMonth = 31; break;
+                case 4:case 6:case 9:case 11:
+                    daysInMonth = 30; break;
+                case 2:
+                    daysInMonth = 28; break;
+                default:
+                    Console.WriteLine("Invalid month!");
+                    return;
+            }
+
+            if (day < daysInMonth)
+
+            {
+                day += 1;
+            }
+            else
+            {
+                day = 1;
+                month += 1;
+                if (month > 12)
+                {
+                    month = 1;
+                }
+            }
+            Console.WriteLine($"Sldushiy den {day + "."+ month}");
+        }
+    }
+}
